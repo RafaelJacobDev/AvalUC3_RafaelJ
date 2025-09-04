073e3d6a3a42818315dd61416139e26c2de83de0
RAFAEL JACOB TEIXEIRA GRANGEIRO <rafael.jtgrangeiro@senacsp.edu.br>
2025-09-04 17:40:27 -0300

    array implemantado

---
+++ impar/pags/impar.php
@@ -0,0 +1,49 @@
+<?php
+$numero1 = filter_input(INPUT_GET, "numero1", filter: FILTER_VALIDATE_INT);
+$numero2 = filter_input(INPUT_GET, "numero2", filter: FILTER_VALIDATE_INT);
+$numero3 = filter_input(INPUT_GET, "numero3", filter: FILTER_VALIDATE_INT);
+$par = 0;
+$impar = 0;
+
+
+$resultado1 = $numero1 % 2;
+$resultado2 = $numero2 % 2;
+$resultado3 = $numero3 % 2;
+
+$resultados = [$resultado1, $resultado2, $resultado3];
+
+if ($resultado1 == 0 || $resultado2 == 0 || $resultado3 == 0) {
+
+    for ($i = 0; $i < count($resultados); $i++) {
+        $par = $par + 1;
+    }
+} elseif ($resultado1 != 0 || $resultado2 != 0 || $resultado3 != 0) {
+    for ($i = 0; $i < count($resultados); $i++) {
+        $impar = $impar + 1;
+    }
+}
+
+$mensagem = "Tem ".$par." numeros pares e ".$impar." numeros impar.";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Impar ou par</title>
+    <link rel="stylesheet" href="./../css/estilo.css">
+</head>
+
+<body>
+    <h1>Resultado</h1>
+    <div id="resultado">
+
+        <?= $mensagem; ?>
+
+    </div>
+</body>
+
+</html>
